c2ab5e5cc5e6fe84a9a51e3c8a27110729f4c537
Milosz B <miloszbargiel@gmail.com>
2022-12-06 12:10:48 +0100

    Day 6 solutions

---
+++ day6.py
@@ -0,0 +1,19 @@
+def find_signal():
+    data = list(open('day6.txt', 'r').read())
+    for i in range(len(data) - 3):
+        possible_marker = set(data[i:i+4])
+        if len(possible_marker) == 4:
+            return i+4
+
+
+def find_message():
+    data = list(open('day6.txt', 'r').read())
+    for i in range(len(data) - 14):
+        possible_message = set(data[i:i+14])
+        print(possible_message)
+        if len(possible_message) == 14:
+            return i + 14
+
+
+print(find_signal())
+print(find_message())
